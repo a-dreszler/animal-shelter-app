dca461146f7e0d6826d64050b3f25d377c14bf7b
Aleksander Dreszler <olek.d93@gmail.com>
2023-06-22 15:30:36 +0200

    Project created

---
+++ src/main/java/pl/adreszler/animalshelterapp/AnimalShelterAppApplication.java
@@ -0,0 +1,13 @@
+package pl.adreszler.animalshelterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalShelterAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalShelterAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/adreszler/animalshelterapp/AnimalShelterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.adreszler.animalshelterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalShelterAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
